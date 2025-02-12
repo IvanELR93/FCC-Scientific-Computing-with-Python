44068683f06468591331c9ff7ca2341bd256108c
Ivan Rodrigues <ivan.elr93@gmail.com>
2025-02-12 09:26:19 -0300

    21 to 50

---
+++ Cipher_30.py
@@ -0,0 +1,9 @@
+text = 'Hello World'
+text[0] = "Z"
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
